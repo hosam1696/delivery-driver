d5524203bc2133a66225415c575768e39bfe1f42
hosam1696 <hosamelnabawy1996@gmail.com>
2019-02-26 19:31:46 +0200

    change app icon , add request page

--- src/pages/requests/requests.ts
+++ src/pages/requests/requests.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the RequestsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,12 +8,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'requests.html',
 })
 export class RequestsPage {
-
+  isRecievingRequests:boolean;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RequestsPage');
+  }
+
+  onToggleChange(event) {
+    console.log(event);
   }
 
 }
