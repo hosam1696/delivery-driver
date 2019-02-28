3f60bc1b8038c5846ffe72d219fbd9aa500f0119
hosam1696 <hosamelnabawy1996@gmail.com>
2019-02-28 17:12:51 +0200

    Get company name in driver profile
    create its types

--- src/providers/types/app-types.ts
+++ src/providers/types/app-types.ts
@@ -14,8 +14,8 @@ export enum appLangs {
 }
 
 export type Langs = 'ar' | 'en';
-export interface UserData {
-  
+
+export interface UserData {  
     id: number,
     "fullName": string,
     "userName": string,
@@ -33,4 +33,48 @@ export interface UserData {
     "long": string | number,
     "api_token": string,
     "player_id": string,
+    company?: Company
+}
+
+export interface Company {
+  id: number;
+  role_id: number;
+  name: string;
+  username: string;
+  email: string;
+  avatar: string;
+  phone: string;
+  gender?: any;
+  birth_date?: any;
+  speciality_id: number;
+  account_type: string;
+  company_type: string;
+  years: string;
+  bank_number?: any;
+  country?: any;
+  state?: any;
+  city?: any;
+  pay_date: string;
+  allow_date: string;
+  bio?: any;
+  lat: number;
+  long: number;
+  is_active: number;
+  email_active: number;
+  logo: string;
+  settings?: any;
+  created_at: string;
+  updated_at: string;
+  player_id?: any;
+  range?: any;
+  logisticsCompanyId?: any;
+  lastMessageDate?: any;
+  unreadMessagesWithMe?: any;
+  avgRate: number;
+  country_name: string;
+  state_name: string;
+  city_name: string;
+  ordersCount: number;
+  status?: any;
+  is_new?: any;
 }
